3a7dba1e3cf874a8f7b705780c11a166966574a5
Nguyễn Hữu Thắng <nguyenhuuthang011@gmail.com>
2025-07-09 12:56:50 +0700

    first commit

---
+++ src/main/java/vn/scime/nhakhoa/NhakhoaApplication.java
@@ -0,0 +1,13 @@
+package vn.scime.nhakhoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhakhoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhakhoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/scime/nhakhoa/NhakhoaApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.scime.nhakhoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhakhoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
